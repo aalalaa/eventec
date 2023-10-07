d00d94d744b47fea8c989e00b2f27c517f6cbb99
aalalaa <112023441+aalalaa@users.noreply.github.com>
2023-10-07 13:31:19 -0300

    Update sobre_view.dart

--- lib/view/sobre_view.dart
+++ lib/view/sobre_view.dart
@@ -11,9 +11,16 @@ class SobreView extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            _buildPersonCard('André Carneiro', 'RA 2840 4822 13006', 'https://cdn-icons-png.flaticon.com/512/3884/3884851.png'),
+            _buildPersonCard('André Filipe', 'RA 2840 4822 13006', 'https://cdn-icons-png.flaticon.com/512/3884/3884851.png'),
             SizedBox(height: 20),
             _buildPersonCard('Julia Mateus', 'RA 2840 4822 13031', 'https://cdn-icons-png.flaticon.com/512/3532/3532661.png'),
+            SizedBox(height: 30),
+            Text(
+              'O EvenTec, um aplicativo multiplataforma para eventos acadêmicos da FATEC-RP. '
+              'Ele serve para visualizar e cadastrar eventos relacionados à unidade de Ribeirão Preto.'
+              'Em resumo, o EvenTec otimiza a experiência acadêmica ao consolidar informações no app e facilitar a participação em eventos universitários.',
+              textAlign: TextAlign.center,
+            ),
           ],
         ),
       ),
